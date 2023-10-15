d09d3feb63b295592090d07675dcc8e3bd751eeb
tyuiu.KushnerovIA@outlook.com <tyuiu.KushnerovIA@outlook.com>
2023-10-15 20:44:08 +0500

    Task5

---
+++ Tyuiu.KushnerovIA.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KushnerovIA.Sprint1.Task5.V4.Lib;
+
+namespace Tyuiu.KushnerovIA.Sprint1.Task5.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValueSecondsToHours()
+        {
+            DataService ds = new DataService();
+            int time = 3600;
+            var res = time / 3600;
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.KushnerovIA.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KushnerovIA.Sprint1.Task5.V4.Lib;
+
+namespace Tyuiu.KushnerovIA.Sprint1.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Кушнеров И. А. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Кушнеров Иван Антонович | ПКТб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу: идет k-ая секунда  *");
+            Console.WriteLine("* суток. Определить, сколько полных часов (h) прошло к этому моменту.     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите k-ую секунду суток: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Полное количество часов = " + ds.SecondsToHours(x));
+            Console.ReadLine();
+        }
+    }
+}
